fc13f61bf492da1fbab48ca9405b019251037152
B-T-D <bdye71@gmail.com>
2021-05-07 14:09:38 -0400

    Previous test.sh version

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ echo $"Running all pytests in Python env."
 
 source env/bin/activate
 
-python3 -m pytest --cov=python_backend
+python3 -m pytest --cov=python_backend # coverage report misses some files even though the corresponding tests ran
