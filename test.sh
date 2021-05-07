c6b04e9ade301c7e50b8b2d2681dc269d30e8e03
B-T-D <bdye71@gmail.com>
2021-05-07 14:11:40 -0400

    Fixed script to print correct coverage report and show missing lines

--- test.sh
+++ test.sh
@@ -4,4 +4,6 @@ echo $"Running all pytests in Python env."
 
 source env/bin/activate
 
-python3 -m pytest --cov=python_backend # coverage report misses some files even though the corresponding tests ran
+coverage run -m pytest && coverage report -m --omit=env/*
+
+#python3 -m pytest --cov=python_backend # old
