00e79b566069cb6edfa471452caac3984a5eaa4a
B-T-D <bdye71@gmail.com>
2021-05-30 01:14:36 -0400

    Hello world JS pipe working for arbitrary JSON

--- http-server/namedPipesUtil.js
+++ http-server/namedPipesUtil.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const { spawn, fork } = require('child_process');
+const { spawn } = require('child_process');
+const { response } = require('express');
 
 
 const path_a = 'fifo_node_to_python';
@@ -15,6 +16,84 @@ const MOCK_REQUEST_JSON = {
     "json_arg": {
         "user_id": "1"
     }
+}
+
+let responseData = null; /* TODO this seems super ugly (setting this global and then having a while loop wait for it to not be null),
+would be better to at least make it a class; best practice is prob to use async stuff */
+
+let pipeIn = spawn('mkfifo', [path_b]);
+pipeIn.on('exit', () => {
+    console.log('Created Node inbound pipe (DB->Web)');
+    
+    
+    
+    
+})
+
+function readFromPipe() {
+    console.log(`readFromPipe was called`)
+    
+    let response = null;
+    
+    
+    while (!response) {
+        readFromPipe();
+    }
+    console.log(`got a response = ${response}`);
+    return response;
+
+}
+
+
+
+function transmitViaPipe(requestData) {
+    const fd = fs.openSync(path_b, 'r+');
+    let fifoReadStream = fs.createReadStream(null, { fd });
+    let fifoWriteStream = fs.createWriteStream(path_a);
+    console.log('Node process created writeStream');
+    fifoWriteStream.write(JSON.stringify(requestData));
+
+    return new Promise((resolve, reject) => {
+        fifoReadStream.on('data', (data) => {
+            console.log(`received from python: ${data.toString()}`);
+            resolve(data.toString())
+        })
+    })        
+    
+    
+    
+    var responseData = null;
+    fifoReadStream.on('data', (data) => { // Need this listener in place before the request is written to the pipe, so it'll be there when response comes (?)
+        console.log(`received from Python: ${data.toString()}`)
+        responseData = data.toString();
+        console.log(`responseData is now ${responseData}`);
+        // console.log(`resolve line about to run`);
+        // resolve(responseData);
+        // console.log(`line after resolve line`);
+    })
+    console.log(`this line ran, response data was ${responseData}`);
+    fifoReadStream.on('end', function () {
+        console.log(`*****in fiforeadstream.on end: responseData = ${responseData}`)
+    })
+    
+    
+}
+
+/**
+ * 
+ * @param {object} requestData : JS object parseable by the database layer interface
+ * @returns {object} : JS object for use in HTTP response JSON body
+ */
+async function databaseRequest(requestData) {
+
+    console.log(`databaseRequest() function called with requestData = ${JSON.stringify(requestData)}`);
+    var response = await transmitViaPipe(requestData)
+    console.log(`response just returned to data`)
+
+    console.log(`transmitViaPipe finished`)
+    console.log(`in databaseRequest(): response = ${response}`)
+    return response;
+    
 }
 
 function pipeHelloWorld() {
@@ -47,7 +126,6 @@ function pipeHelloWorld() {
     });
 }
 
-pipeHelloWorld()
 
 /* Temp mock function to call */
 
@@ -71,4 +149,15 @@ function mockPipeOutput(dataIn) {
     } 
 }
 
-module.exports = usePipe;
+var mockDataIn = {
+    "method": "get_next_candidate",
+    "json_arg": {
+        "user_id": "1"
+    }
+}
+
+var jsonResponse = databaseRequest(mockDataIn)
+console.log(jsonResponse)
+
+module.exports = databaseRequest;
+//module.exports = usePipe;
--- http-server/routes/candidates.js
+++ http-server/routes/candidates.js
@@ -1,18 +1,28 @@
 var express = require('express');
+const pipeHelloWorld = require('../namedPipesUtil');
 
 const namedPipesInterface = require('../namedPipesUtil')
+
+const databaseRequest = require('../namedPipesUtil');
 
 var candidatesRouter = express.Router();
 
 
 var mockDataIn = {
-    "method": "get_next_candidate"
+    "method": "get_next_candidate",
+    "json_arg": {
+        "user_id": "1"
+    }
 }
 
 candidatesRouter.get('/', function(req, res, next) {
-    console.log('candidates router called');
-    var pipesDataOut = namedPipesInterface(mockDataIn);
-    res.send(pipesDataOut)
+    //pipeHelloWorld();
+    var responseJSON = databaseRequest(mockDataIn);
+    console.log(`in candidates router: responseJSON = ${responseJSON}`);
+    next()
+    //console.log('candidates router called');
+    //var pipesDataOut = namedPipesInterface(mockDataIn);
+    //res.send(pipesDataOut)
     //res.send('hello world from candidates router');
 })
 
