1b026f14557ff798d1788b7abab99f5443660983
B-T-D <bdye71@gmail.com>
2021-06-07 17:25:03 -0400

    Testing infrastructure setup

---
+++ http-server/test/test_api_endpoints.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const { get } = require('../routes/users');
+
+const server = require('../server');
+
+describe('API root URL', () => {
+    describe('GET request', () => {
+        it('returns a 200 status to indicate that the server is online', async () => {
+            
+            // Exercise
+            const response = await request(server)
+            .get('api/v1');
+
+            // Verify
+            assert.equal(response.status, 200);
+
+
+        })
+    })
+})
+
