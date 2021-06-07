5161f53a6b767b8ec9243ce298f7f8b2f4ea045f
B-T-D <bdye71@gmail.com>
2021-06-07 19:15:07 -0400

    Unbroke review.py tests

--- test/test_review.py
+++ test/test_review.py
@@ -39,7 +39,7 @@ class TestHelloWorldThings(unittest.TestCase):
         self.terrezanos_price_range = 2
         self.terrezanos_hours = [[14, 22], [14, 21], [14, 21], [14, 21], [14, 23], [14, 23], [14, 20]] # ints in [0..23] representing hours, for now
 
-        terrezanos_json = json.dumps({
+        terrezanos_data = json.dumps({
             "location" : self.terrezanos_location,
             "name" : self.terrezanos_name,
             "traits" : self.terrezanos_traits,
@@ -54,7 +54,8 @@ class TestHelloWorldThings(unittest.TestCase):
 
         # Connect to the database with the mock data set
         self.db = DatabaseAPI(json_map_filename=TEST_JSON_DB_NAME)
-        self.terrezanos_id = self.db.post_object("datespot", terrezanos_json)
+        json_arg = json.dumps({"object_model_name": "datespot", "json_data": terrezanos_data})
+        self.terrezanos_id = self.db.post_object(json_arg)
 
         # Instantiate mock Review object
 
