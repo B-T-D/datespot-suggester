130421601feabaddc29dca7389fe49b86348e16c
B-T-D <bdye71@gmail.com>
2021-09-23 17:52:00 -0400

    Tidy comments

--- test.sh
+++ test.sh
@@ -4,9 +4,4 @@ echo $"Running all pytests in Python env"
 
 source env/bin/activate
 
-python3 -m populate_mock_data && coverage run -m pytest -s && coverage report -m --omit=env/* # -s flag shows std out even when tests pass (?)
-
-#python3 -m pytest --cov=python_backend # old
-# Scratch:
-# python3 -m pytest test/test_google_places_api_client.py
-
+python3 -m populate_mock_data && coverage run -m pytest -s && coverage report -m --omit=env/* # -s flag shows std out even when tests pass
